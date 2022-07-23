e4bc47dc29486f1ea4fab7cc3e73b9e097d3cfe9
ctyuucsdedu <ctyu@ucsd.edu>
2022-07-23 12:34:41 -0700

    v2.3

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # cse134-hw4
 # Name: Kevin C. Yu
 # PID: A16548882
+
+# Link: 
--- customdialog.js
+++ customdialog.js
@@ -1,4 +1,4 @@
-export function alert_function(e) {
+export function alert_function2(e) {
     document.getElementById("div1").hidden = false;
     
     const ok = document.getElementById("ok1");
@@ -10,12 +10,12 @@ export function alert_function(e) {
     e.preventDefault();
 }
 
-export function confirm_function(e) {
+export function confirm_function2(e) {
     document.getElementById("div2").hidden = false;
     
     const ok = document.getElementById("ok2");
     const cancel = document.getElementById("cancel2");
-    const output = document.getElementById("output");
+    const output = document.getElementById("output2");
 
     ok.addEventListener('click', () => {
         output.innerHTML = `The value returned by the confirm method is : true`;
@@ -29,12 +29,12 @@ export function confirm_function(e) {
     e.preventDefault();
 }
 
-export function prompt_function(e) {
+export function prompt_function2(e) {
     document.getElementById("div3").hidden = false;
     
     const ok = document.getElementById("ok3");
     const cancel = document.getElementById("cancel3");
-    const output = document.getElementById("output");
+    const output = document.getElementById("output2");
 
     ok.addEventListener('click', () => {
         const dirty = document.getElementById("prompt_input");
